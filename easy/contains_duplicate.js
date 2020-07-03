647b88e8939f80e126dbd69da86e8bd3b385d392
Jeffery Jones <56287988+jeff-jones2020@users.noreply.github.com>
2020-07-02 23:50:06 -0700

    added solution for contains_duplicate

---
+++ easy/contains_duplicate.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  let map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map[nums[i]] !== undefined) {
+      return true;
+    }
+    map[nums[i]] = null;
+  }
+
+  return false;
+};
