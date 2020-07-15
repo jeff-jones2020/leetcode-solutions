1a4ddabc1d049f7f19763612f88d34351590b280
Jeffery Jones <56287988+jeff-jones2020@users.noreply.github.com>
2020-07-14 21:52:02 -0700

    Added solution for merge_two_sorted_lists

---
+++ easy/merge_two_sorted_lists.js
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (l1, l2) {
+  if (!l1 && !l2) return l1;
+  if (!l1) return l2;
+  if (!l2) return l1;
+
+  let newHead;
+  let curNode;
+  if (l1.val < l2.val) {
+    newHead = l1;
+    l1 = l1.next;
+  } else {
+    newHead = l2;
+    l2 = l2.next;
+  }
+  curNode = newHead;
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      curNode.next = l1;
+      l1 = l1.next;
+    } else {
+      curNode.next = l2;
+      l2 = l2.next;
+    }
+    curNode = curNode.next;
+  }
+
+  if (l1) {
+    while (l1) {
+      curNode.next = l1;
+      l1 = l1.next;
+      curNode = curNode.next;
+    }
+  } else if (l2) {
+    while (l2) {
+      curNode.next = l2;
+      l2 = l2.next;
+      curNode = curNode.next;
+    }
+  }
+
+  return newHead;
+
+};
